4eb5c8a7f45556a5656904b9711ce65242f1feb2
twinh <twinhuang@qq.com>
2020-02-09 23:35:40 +0800

    refactor: 拆分出 auto-import-name

---
+++ auto-import-name.js
@@ -0,0 +1,41 @@
+module.exports = function () {
+  // eslint-disable-next-line no-undef
+  const visited = Symbol('visited');
+  const getImportArgPath = p => p.parentPath.get('arguments')[0];
+  const trimChunkName = baseDir => baseDir.replace(/^[./]+|(\.js$)/g, '');
+
+  function getMagicCommentChunkName(importArgNode) {
+    const {quasis, expressions} = importArgNode;
+    if (!quasis) {
+      return trimChunkName(importArgNode.value);
+    }
+
+    const baseDir = quasis[0].value.cooked;
+    const hasExpressions = expressions.length > 0;
+    const chunkName = baseDir + (hasExpressions ? '[request]' : '');
+    return trimChunkName(chunkName);
+  }
+
+  function promiseAll(p) {
+    const argPath = getImportArgPath(p);
+    const importArgNode = argPath.node;
+    const chunkName = getMagicCommentChunkName(importArgNode);
+
+    if (!argPath.node.leadingComments) {
+      argPath.addComment('leading', ` webpackChunkName: '${chunkName}' `);
+    }
+  }
+
+  return {
+    name: 'auto-import-name',
+    visitor: {
+      Import(p) {
+        if (p[visited]) {
+          return;
+        }
+        p[visited] = true;
+        promiseAll(p);
+      }
+    }
+  };
+};
